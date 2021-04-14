8a55f856943349a71221907475813bfae502938e
David Godwin <davidgodwin@MacBook-Pro.frontierlocal.net>
2021-04-14 16:09:37 -0400

    Add repositories

---
+++ SnackStore.Domain/Repositories/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SnackStore.Domain.Contexts;
+using SnackStore.Domain.Model;
+
+namespace SnackStore.Domain.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly ISnackStoreContext _context;
+
+        public CategoryRepository(ISnackStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _context.Categories;
+        }
+    }
+}
---
+++ SnackStore.Domain/Repositories/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SnackStore.Domain.Contexts;
+using SnackStore.Domain.Model;
+
+namespace SnackStore.Domain.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly ISnackStoreContext _context;
+
+        public ProductRepository(ISnackStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products;
+        }
+    }
+}
